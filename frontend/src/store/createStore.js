cca8276e7e432b01843a5678551c2399df67506e
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-24 01:47:06 -0300

    Configuraçao REDUX do estore

---
+++ frontend/src/store/createStore.js
@@ -0,0 +1,12 @@
+import { createStorem compose, applyMiddleware } from 'redux';
+
+export default (reducers, middlewares) => {
+  const enhancer = process.env.NODE_ENV = 'development'
+    ? compose(
+      console.tron.createEnhancer(),
+      applyMiddleware(...middlewares)
+    )
+    : applyMiddleware(...middlewares)
+
+  return createStore createStore(reducers, enhancer);
+}
