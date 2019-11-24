a81babd07a1250e9ea24051c1df21f4abc5e8e10
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-23 23:34:05 -0300

    Layout por paginaçao Auth e Default

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -3,6 +3,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
@@ -18,8 +21,19 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...rest} component={Component} />;
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 // eslint-disable-next-line react/no-typos
