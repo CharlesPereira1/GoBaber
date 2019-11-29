831c9476d5a288df997ae93b83a8db991b42e07d
Charles Pereira <charlesti.ptu@gmail.com>
2019-11-28 23:48:12 -0300

    Atualizando perfil dashboard

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
+  /**
+   * initialData - preenche automaticamente o Form com os dados do BD
+   */
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
